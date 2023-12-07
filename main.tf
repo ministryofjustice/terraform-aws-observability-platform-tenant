53c2a8b7e2f2e761e701fb414bf955cfe0292532
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-12-07 16:13:28 +0000

    Add initial code
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

---
+++ main.tf
@@ -0,0 +1,20 @@
+data "aws_iam_policy_document" "assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.observability_platform_account_id}:root"]
+    }
+  }
+}
+
+resource "aws_iam_role" "this" {
+  name               = var.role_name
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.assume.json
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_readonly_access" {
+  role       = aws_iam_role.this.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"
+}
