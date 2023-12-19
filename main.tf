0ec3ddcf6907d5bfabdadc6792b51f2ed7c86ae6
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-12-19 15:05:48 +0000

    🩻 Add AWS X-Ray (#5)
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- main.tf
+++ main.tf
@@ -20,3 +20,10 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_readonly_access" {
   role       = aws_iam_role.this.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"
 }
+
+resource "aws_iam_role_policy_attachment" "xray_readonly_access" {
+  count = var.enable_xray ? 1 : 0
+
+  role       = aws_iam_role.this.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSXrayReadOnlyAccess"
+}
