2d21610cdd38bfc89ea787a3abba79f3c7e197d4
Gary H <26419401+Gary-H9@users.noreply.github.com>
2024-10-01 15:50:41 +0000

    :wrench: Add custom athena policy

--- main.tf
+++ main.tf
@@ -8,6 +8,64 @@ data "aws_iam_policy_document" "assume" {
   }
 }
 
+data "aws_iam_policy_document" "custom_athena_policy" {
+  #checkov:skip=CKV_AWS_356: Needs to access multiple resources
+  #checkov:skip=CKV_AWS_111
+  statement {
+    sid    = "AthenaQueryAccess"
+    effect = "Allow"
+    actions = [
+      "athena:ListDatabases",
+      "athena:ListDataCatalogs",
+      "athena:ListWorkGroups",
+      "athena:GetDatabase",
+      "athena:GetDataCatalog",
+      "athena:GetQueryExecution",
+      "athena:GetQueryResults",
+      "athena:GetTableMetadata",
+      "athena:GetWorkGroup",
+      "athena:ListTableMetadata",
+      "athena:StartQueryExecution",
+      "athena:StopQueryExecution"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "GlueReadAccess"
+    effect = "Allow"
+    actions = [
+      "glue:GetDatabase",
+      "glue:GetDatabases",
+      "glue:GetTable",
+      "glue:GetTables",
+      "glue:GetPartition",
+      "glue:GetPartitions",
+      "glue:BatchGetPartition"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "AthenaS3Access"
+    effect = "Allow"
+    actions = [
+      "s3:GetBucketLocation",
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:ListBucketMultipartUploads",
+      "s3:ListMultipartUploadParts",
+      "s3:AbortMultipartUpload",
+      "s3:PutObject"
+    ]
+    resources = ["arn:aws:s3:::${var.grafana_athena_query_results_s3_bucket}"]
+  }
+  statement {
+    sid       = "AthenaCostUsageReportsAccess"
+    effect    = "Allow"
+    actions   = ["s3:GetObject", "s3:ListBucket"]
+    resources = ["arn:aws:s3:::${var.cost_usage_reports_s3_bucket}"]
+  }
+}
+
 resource "aws_iam_role" "this" {
   name               = var.role_name
   path               = "/"
@@ -35,19 +93,11 @@ resource "aws_iam_role_policy_attachment" "xray_readonly_access" {
   policy_arn = "arn:aws:iam::aws:policy/AWSXrayReadOnlyAccess"
 }
 
-# resource "aws_iam_role_policy_attachment" "athena_readonly_access" {
-#   count = var.enable_athena ? 1 : 0
-
-#   role       = aws_iam_role.this.name
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonAthenaFullAccess"
-# }
-
-# this is purely for testing, at first glance there is not a read only policy for athena
-resource "aws_iam_role_policy_attachment" "athena_full_access" {
+resource "aws_iam_role_policy_attachment" "athena_custom_access" {
   count = var.enable_athena ? 1 : 0
 
   role       = aws_iam_role.this.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonAthenaFullAccess"
+  policy_arn = data.aws_iam_policy_document.custom_athena_policy
 }
 
 resource "aws_iam_role_policy_attachment" "additional_policies" {
--- variables.tf
+++ variables.tf
@@ -31,20 +31,24 @@ variable "enable_xray" {
 
 variable "enable_athena" {
   type        = bool
-  description = "Enable AWS Athena Full managed policy"
+  description = "Enable AWS Athena custom policy"
   default     = false
 }
-
-# variable "enable_athena" {
-#   type        = bool
-#   description = "Enable custom Athena read only policy"
-#   default     = false
-# }
 
 variable "additional_policies" {
   type        = map(string)
   description = "ARNs of any policies to attach to the IAM role"
   default     = {}
+}
+
+variable "grafana_athena_query_results_s3_bucket" {
+  description = "The S3 bucket for Grafana Athena query results"
+  type        = string
+}
+
+variable "cost_usage_reports_s3_bucket" {
+  description = "The S3 bucket for Cost and Usage reports"
+  type        = string
 }
 
 variable "tags" {
